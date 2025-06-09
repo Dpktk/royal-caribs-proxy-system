7c617767566f0e85b8342121a007111e959e4a8d
Deepak <144918425+Dpktk@users.noreply.github.com>
2025-06-10 00:01:44 +0530

    Committing Project Structures.

---
+++ offshore-proxy/src/main/java/com/royalcaribs/offshoreproxy/RoyalCaribsOffshoreProxyApplication.java
@@ -0,0 +1,13 @@
+package com.royalcaribs.offshoreproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalCaribsOffshoreProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyalCaribsOffshoreProxyApplication.class, args);
+	}
+
+}
---
+++ ship-proxy/src/main/java/com/royalcaribs/shipproxy/RoyalCaribsShipProxyApplication.java
@@ -0,0 +1,13 @@
+package com.royalcaribs.shipproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalCaribsShipProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyalCaribsShipProxyApplication.class, args);
+	}
+
+}
